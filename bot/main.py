a85df7173ddd78d4cf8bf389d781010c00bc419e
+futurehendrix <bochka13@list.ru>
2024-11-26 12:46:58 +0300

    integration a tgbot

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,26 @@
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-from bot.config import BOT_TOKEN
+from aiogram.filters import Command
+from aiogram import F
+import asyncio
+from config import BOT_TOKEN
 
+# Инициализация бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler(commands=["start"])
+# Хэндлер команды /start
+@dp.message(Command("start"))
 async def send_welcome(message: Message):
-    await message.reply("Привет! Я твой To-Do бот. Напиши /help, чтобы узнать, что я умею.")
+    await message.answer("Привет! Я твой To-Do бот. Напиши /help, чтобы узнать, что я умею.")
 
-# Основной запуск бота
+# Основная функция запуска бота
+async def main():
+    try:
+        print("Бот запущен!")
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
