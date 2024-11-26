b4603100c2199348ad8f70da5cb50a8ad796e62e
+futurehendrix <bochka13@list.ru>
2024-11-26 12:19:29 +0300

    update config bot

---
+++ bot/config.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+# Загружаем переменные окружения из файла .env
+load_dotenv()
+
+# Токен Telegram-бота
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Конфигурация базы данных
+DB_CONFIG = {
+    "host": os.getenv("DB_HOST"),
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "port": os.getenv("DB_PORT"),
+}
